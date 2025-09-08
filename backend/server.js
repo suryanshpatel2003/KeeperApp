6a07069d4d5412afe561cbfbbc8921bc3f22d067
Suryansh Patel <patelsuryansh30@gmail.com>
2025-09-09 01:46:14 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,5 @@ app.get("/", (req,res)=>{
     res.send("App is Running")
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
