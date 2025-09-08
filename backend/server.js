c65c549fec6dda1d806bfe9f31b77fb4bb6cbee3
Suryansh Patel <patelsuryansh30@gmail.com>
2025-09-09 01:00:16 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,9 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
+app.get("/", (req,res)=>{
+    res.send("App is Running")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
